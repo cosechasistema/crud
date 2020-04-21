31a9d8850bb3bacda222a1c7d6ee65fb44fafcb8
cosechasistema@gmail.com <admin@dubchristian>
2020-04-21 00:41:56 +0000

    maestro detalle

--- app/Http/Controllers/AdminOrdersController.php
+++ app/Http/Controllers/AdminOrdersController.php
@@ -13,7 +13,7 @@ class AdminOrdersController extends CBController {
 
         $this->addDatetime("Created At","created_at")->required(false)->showAdd(false)->showEdit(false);
 		$this->addDatetime("Updated At","updated_at")->required(false)->showAdd(false)->showEdit(false);
-		$this->addSelectTable("Customer","customers_id",["table"=>"customers","value_option"=>"id","display_option"=>"name","sql_condition"=>""])->foreignKey('id');
+		$this->addSelectTable("Customer","customers_id",["table"=>"customers","value_option"=>"id","display_option"=>"name","sql_condition"=>""])->help("Ingrese el cliente")->foreignKey('id');
 		$this->addText("Order Number","order_number")->strLimit(150)->maxLength(255);
 		$this->addNumber("Total","total");
 		
